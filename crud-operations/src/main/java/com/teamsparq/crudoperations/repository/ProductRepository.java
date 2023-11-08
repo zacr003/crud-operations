8532698b5cbe95c61b3c274c81313f0e01448a09
zac.ramsey <zac.ramsey@teamsparq.com>
2023-11-08 14:23:27 -0700

    Adds Controller, Entity, and Repository for Product / adds simple HTML templates

---
+++ crud-operations/src/main/java/com/teamsparq/crudoperations/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.teamsparq.crudoperations.repository;
+
+import com.teamsparq.crudoperations.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
